5b5c38f5b65b87be0fb88fea600dca7e562f5c10
Doszhan <doszhan.m@mail.ru>
2022-09-27 11:08:48 +0600

    add signal

---
+++ backend/backend/accounts/signals.py
@@ -0,0 +1,13 @@
+from django.dispatch import receiver
+from django.db.models.signals import (
+    post_save, pre_save, post_delete
+)
+
+from .models import User
+
+
+@receiver(post_save, sender=User)
+def signal_test(sender, instance: User, created, update_fields, *args, **kwargs):
+    ''' Test signal.
+    '''
+    print('user post_save test signal')
