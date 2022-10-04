8626af553bdaf688eed135c933a17a0420bfe511
Doszhan <doszhan.m@mail.ru>
2022-10-04 23:04:19 +0600

    point

--- backend/backend/main/admin.py
+++ backend/backend/main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Channel, Video, Comment, 
+    SubscribeChannel, LikeVideo
+)
+
+
+admin.site.register(Video)
+admin.site.register(Channel)
+admin.site.register(Comment)
+admin.site.register(LikeVideo)
+admin.site.register(SubscribeChannel)
+
