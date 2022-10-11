9045920e7145de92962838fb4a17569908ce5c0b
Doszhan <doszhan.m@mail.ru>
2022-10-11 11:30:03 +0600

    video seacrh

--- backend/backend/main/documents.py
+++ backend/backend/main/documents.py
@@ -1,5 +1,24 @@
+from django_elasticsearch_dsl import Document, fields
+from django_elasticsearch_dsl.registries import registry
+
+from .models import Video
 
 
+@registry.register_document
+class VideoDocument(Document):
+    id = fields.IntegerField()
 
+    class Index:
+        name = 'videos'
+        settings = {
+            'number_of_shards': 1,
+            'number_of_replicas': 0,
+        }
 
-        
+    class Django:
+        model = Video
+        fields = [
+            'title',
+            'description',
+            'hashtag',
+        ]        
--- backend/backend/main/views.py
+++ backend/backend/main/views.py
@@ -1,10 +1,8 @@
 from elasticsearch_dsl import Q
 
 from rest_framework.generics import ListAPIView
-from .managers.elastic_manager import (
-    PaginatedElasticSearchAPIView, VideoDocument
-)
 
+from .documents import VideoDocument
 from .serializers import VideoSerializer
 
 
@@ -12,11 +10,13 @@ class SearchVideo(ListAPIView):
     serializer_class = VideoSerializer
 
     def get_queryset(self, *args, **kwargs):
-        query = self.kwargs['query']
-        que = Q('multi_match', query=query,
-            fields=['title', 'description', 'hashtag',
-            ], fuzziness='auto')
-        search = VideoDocument.search().query(que)
-        q = search.to_queryset()
-        return q
-        
+        value = self.kwargs['query']
+        query = Q('multi_match', query=value,
+                fields=[
+                    'title',
+                    'description',
+                    'hashtag',
+                ], fuzziness='auto')
+        search = VideoDocument.search().query(query)
+        queryset = search.to_queryset()
+        return queryset
