903a48388c7a92242bed57517fb1c424e05e5d18
Doszhan <doszhan.m@mail.ru>
2022-10-17 23:27:17 +0600

    x user midlware

--- backend/backend/root/settings.py
+++ backend/backend/root/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'root.middleware.UserSubMiddleware',
 ]
 
 
