27cdf33f4eb00a677bb79e9fa3328654bd650688
Doszhan <doszhan.m@mail.ru>
2022-11-17 11:52:28 +0600

    certbot

--- backend/backend/run_app.sh
+++ backend/backend/run_app.sh
@@ -3,12 +3,12 @@
 
 python3 manage.py migrate
 python3 manage.py createcachetable
-# python3 manage.py collectstatic --noinput
-python3 manage.py search_index --rebuild --parallel --force-color
+# python3 manage.py search_index --rebuild --parallel --force-color
 
 if [ "$DEPLOY" = "TRUE" ]; then
     echo "------------------------------------------------------------------------------------"
     echo "RUNNING DEPLOY"
+    python3 manage.py collectstatic --noinput
     echo "------------------------------------------------------------------------------------"
     gunicorn --workers 4 --threads 4 root.wsgi --bind [::]:8000 --log-level=debug \
      --access-logfile '-' --error-logfile '-' \
