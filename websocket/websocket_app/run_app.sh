a747ec6834016072a3e0215effb96565a7b360ef
Doszhan <doszhan.m@mail.ru>
2022-11-30 20:18:25 +0600

    websocket

--- websocket/websocket_app/run_app.sh
+++ websocket/websocket_app/run_app.sh
@@ -3,7 +3,7 @@
 
 if [[ "$DEPLOY" = "TRUE" ]]; then
     echo "RUNNING DEPLOY"
-    uvicorn main:app --host 0.0.0.0 --port 8000 --workers 3
+    uvicorn main:app --host 0.0.0.0 --port 8000 --workers 2
 else
     echo "RUNNING TEST"
     uvicorn main:app --host 0.0.0.0 --port 8000 --workers 1 --reload
